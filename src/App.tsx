649701f60a2572326cefe55d5f8793f22579323e
Bryan <bryanfg@hotmail.es>
2025-12-24 12:53:58 -0500

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,23 @@ import { PrivacyPolicy } from '@/presentation/pages/PrivacyPolicy';
 import { TaxCalculatorPage } from '@/presentation/pages/TaxCalculatorPage';
 import { Route, Routes } from 'react-router-dom';
 
+import { Analytics } from '@vercel/analytics/react';
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<MainLayout />}>
-        <Route index element={<Home />} />
-        <Route path="calculadora-retefuente" element={<TaxCalculatorPage />} />
-        <Route path="calculadora-credito-hipotecario" element={<LoanCalculatorPage />} />
-        <Route path="simulador-inversion" element={<InvestmentCalculatorPage />} />
-        <Route path="privacidad" element={<PrivacyPolicy />} />
-        <Route path="terminos" element={<PrivacyPolicy />} /> {/* Temporary setup */}
-      </Route>
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="calculadora-retefuente" element={<TaxCalculatorPage />} />
+          <Route path="calculadora-credito-hipotecario" element={<LoanCalculatorPage />} />
+          <Route path="simulador-inversion" element={<InvestmentCalculatorPage />} />
+          <Route path="privacidad" element={<PrivacyPolicy />} />
+          <Route path="terminos" element={<PrivacyPolicy />} /> {/* Temporary setup */}
+        </Route>
+      </Routes>
+      <Analytics />
+    </>
   );
 }
 
